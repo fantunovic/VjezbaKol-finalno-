9e03eb060a217791c4181c0b85104a6233219947
fantunovic <91869005+fantunovic@users.noreply.github.com>
2021-11-12 19:17:06 +0100

    Create BladeRunner.js

---
+++ screens/BladeRunner.js
@@ -0,0 +1,127 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
+
+
+export function BladeRunner({route, navigation}) {
+
+    function handleHomePress(){
+        navigation.navigate("Filmovi")
+    }
+
+    return (
+        <View style={styles.container}>
+
+<View style={styles.jumbotron}>
+        <View style={styles.jumbotron}>
+        
+        
+        <Image style={styles.image} source={require("../assets/film2.jpg")}/>
+        <Text style={styles.text}>A blade runner must pursue and terminate four replicants who stole a ship in space, and have returned to Earth to find their creator.</Text>
+        <Text style={styles.text}>Director: Ridley Scott</Text>
+        <Text style={styles.text}>Starring: Harrison Ford, Rutger Hauer, Sean Young</Text>
+        <Text style={styles.text}>Rating: 4/5</Text>
+
+    
+    
+        </View>
+       
+      </View>
+
+        
+            <Button 
+            title="Back to Movies"
+            onPress={handleHomePress}
+            />
+        </View>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#A8B4A5',
+  
+  
+    },
+
+    opisFilm: {
+      fontSize: 10,
+      textAlign: "justify",
+      
+    },
+  
+    headerWrapper: {
+      padding: 10,
+      borderBottomWidth: 2
+    },
+    headerTitle: {
+      textAlign: "center",
+      fontSize: 30
+    },
+  
+    jumbotron: {
+     
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+
+    naslovFilm: {
+      textAlign: "center",
+      fontSize: 20
+    },
+  
+    leftContainer: {
+   
+      backgroundColor: '#A8B4A5',
+      padding: 25
+  
+    },
+  
+    rightContainer: {
+   
+      backgroundColor: '#A8B4A5',
+      padding: 25
+  
+    },
+  
+    rightContainer: {
+   
+      backgroundColor: '#A8B4A5',
+      padding: 25
+  
+    },
+  
+    leftText: {
+      color: "red",
+      fontSize: 30
+  
+    },
+  
+    text: {
+        textAlign: "center",
+        fontSize: 15,
+        padding: 10
+  
+    },
+  
+    image: {
+      width: 220,
+      height:300,
+      
+  
+    },
+  
+    touchableCountainer: {
+      margin: 20,
+      backgroundColor: "white",
+      padding: 20
+    },
+  
+    textStyle: {
+      fontSize: 20
+    }
+  
+  });
